01f6c06466b01717b1974cff702404afbec97364
DURGAPRASAD-67 <119409367+DURGAPRASAD-67@users.noreply.github.com>
2024-12-14 12:15:01 +0530

    Update Test.java

--- src/test/java/Adit/Test.java
+++ src/test/java/Adit/Test.java
@@ -3,9 +3,9 @@ package Adit;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Test {
+public class TestMayaAI {
 
-    @Testing
+    @Test
     public void testDatabaseStatus() {
         MayaAI mayaAI = new MayaAI();
         boolean isActive = mayaAI.checkDatabaseStatus("2000000018", "Thub@123");
