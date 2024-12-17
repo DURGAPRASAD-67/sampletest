89c3b18de312ff5f74fa0f9a1416e2930c3b2307
DURGAPRASAD-67 <119409367+DURGAPRASAD-67@users.noreply.github.com>
2024-12-17 20:04:14 +0530

    Update MayaAITest.java

--- src/test/java/Adit/MayaAITest.java
+++ src/test/java/Adit/MayaAITest.java
@@ -8,6 +8,7 @@ public class MayaAITest {
     @Test
     public void testDatabaseStatus() {
         MayaAI mayaAI = new MayaAI();
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver"
         boolean isActive = mayaAI.checkDatabaseStatus("2000000018", "Thub@123");
         Assert.assertTrue("Database should be active", isActive);
     }
